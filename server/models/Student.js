74e97e305fde3f13ec7175dfa3d415c451f056d4
Avishkar Lokhande <avishkarrl23087@gmail.com>
2024-11-20 19:20:43 +0530

    First Commit

---
+++ server/models/Student.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const studentSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    testId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "Test"
+    },
+    startedAt: {
+        type: Date,
+        default: Date.now()
+    },
+    submittedAt: {
+        type: Date,
+    },
+    submissions: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Submission"
+        }
+    ]
+})
+
+
+module.exports = mongoose.model("Student", studentSchema)
